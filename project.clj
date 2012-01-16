4e50d9cc038b3835d32562724b4660388a18af1b
Dave Sann <davesann.p@gmail.com>
2012-01-17 09:53:02 +1100

    Removed local repo proxy

--- project.clj
+++ project.clj
@@ -1,13 +1,6 @@
 (defproject dsann/dsann-clj-utils "0.1.0"
   :description "general utilities"
                  
-  :omit-default-repositories true
-  :repositories {"global-releases"  "http://artifactory/artifactory/libs-release"
-                 "global-snapshots" "http://artifactory/artifactory/libs-snapshot"
-                 "snapshots" "http://artifactory/artifactory/libs-snapshot-local"
-                 "releases"  "http://artifactory/artifactory/libs-release-local"
-                 }
-  
   :dependencies     [[org.clojure/clojure "1.3.0"]
                      
                      [org.clojure/tools.logging "0.2.0"]
